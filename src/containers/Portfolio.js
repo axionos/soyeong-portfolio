d48ba1e22eb9c4c11e7ec90604cf2f384fe3bbe4
axionos <axionoso@gmail.com>
2019-08-07 15:07:49 -0400

    add icon link for all the projects

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -39,6 +39,11 @@ class Portfolio extends React.Component {
                   <TripPlannerSlider />
                   <Modal.Description>
                     <h2 className='project-title'>My Trip Planner</h2>
+                    <div className='project-links'>
+                      <a href='https://github.com/axionos/my-trip-planner-frontend' target='_blank'>
+                        <i class="fab fa-github-square"></i>
+                      </a>
+                    </div>
                     <div className='project-detail'>
                       <p> My Trip Planner is my solo project that allows a user to easily organize daily trip itineraries and display them on the map. It was built with React, Ruby on Rails, Redux, Google Place and Google Maps API.</p>
                       <p>● Developed a Rails API backend with endpoints for users, trips, days, and items</p>
@@ -74,6 +79,11 @@ class Portfolio extends React.Component {
                   <BobaFinderSlider />
                   <Modal.Description>
                     <h2 className='project-title'>Boba Finder</h2>
+                    <div className='project-links'>
+                      <a href='https://github.com/axionos/bobafinder-frontend' target='_blank'>
+                        <i class="fab fa-github-square"></i>
+                      </a>
+                    </div>
                     <div className='project-detail'>
                       <p>By using <span className='bold'>Foursquare API</span>, Bobafinder lets a user to find a bubble tea stores in NYC, filter the search result on different brands, save favorite stores and visited stores on the user profile.</p>
                       <p>● Developed a Rails API backend with endpoints for users, stores, and user-stores </p>
@@ -107,6 +117,11 @@ class Portfolio extends React.Component {
                   <FundRunSlider />
                   <Modal.Description>
                     <h2 className='project-title'>Fund Run</h2>
+                    <div className='project-links'>
+                      <a href='https://github.com/axionos/fundrun-on-rails' target='_blank'>
+                        <i class="fab fa-github-square"></i>
+                      </a>
+                    </div>
                     <div className='project-detail'>
                       <p>Fund Run is marathon charity app built with <span className='bold'>pure Ruby on Rails</span>, which allows a user to follow a marathon they're interested in, make a donation on a charity and check the follow status and donation stat on the user profile page.</p>
                       <p>● Developed Ruby on Rails frontend and backend for users, marathons, charities, follow, and donations</p>
@@ -139,6 +154,11 @@ class Portfolio extends React.Component {
                   <CocktailMenuSlider />
                   <Modal.Description>
                     <h2 className='project-title'>Dope Cocktail Menu</h2>
+                    <div className='project-links'>
+                      <a href='https://github.com/axionos/MOD3_DopeCocktailMenu_FrontEnd' target='_blank'>
+                        <i class="fab fa-github-square"></i>
+                      </a>
+                    </div>
                     <div className='project-detail'>
                       <p>Dope Cocktail Menu is an interactive cocktail recipe menu based on a user's mood. It allows the user to view, like, comment on a recipe, or select a different mood from the menu.</p>
                       <p>● Developed Rails API backend with endpoints for cocktails, comments, mood and likes</p>
@@ -171,7 +191,7 @@ class Portfolio extends React.Component {
                     <h2 className='project-title'>Motor Impact of Canada</h2>
                     <div className='project-links'>
                       <a href='https://www.oebodyparts.com' target='_blank'>
-                        <i class="fas fa-external-link-alt"></i>
+                        <i class="fas fa-external-link-square-alt"></i>
                       </a>
                     </div>
                     <p>● Designed fully responsive modern B2B eCommerce website for the company. </p>
@@ -202,6 +222,11 @@ class Portfolio extends React.Component {
                   <CityGuruSlider />
                   <Modal.Description>
                     <h2 className='project-title'>City Guru</h2>
+                    <div className='project-links'>
+                      <a href='https://github.com/axionos/city-guru-project' target='_blank'>
+                        <i class="fab fa-github-square"></i>
+                      </a>
+                    </div>
                     <div className='project-detail'>
                       <p>City Guru is CLI app built with pure Ruby code. It uses <span className='bold'>Teleport API</span> and allows a user to search for a basic information, salary information, and quality of living information of an urban city the user looks up.</p>
                       <p>● Developed with pure Ruby backend</p>
