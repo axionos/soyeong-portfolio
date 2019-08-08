4f63d12b6fbb2f430f363b636d428256833e4d6e
axionos <axionoso@gmail.com>
2019-08-08 10:15:07 -0400

    add profile photo

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Grid, Segment, Button }from 'semantic-ui-react'
 import ProgressTemplate from '../components/ProgressTemplate';
+import profilePic from '../img/download.png'
 // import { Progress } from 'react-sweet-progress';
 // import "react-sweet-progress/lib/style.css";
 
@@ -14,67 +15,6 @@ class About extends React.Component {
     // this.props.changeStatus()
   }
 
-  move = () => {
-    const html = document.getElementById("htmlBar")
-    const js = document.getElementById("jsBar")
-    const react = document.getElementById("reactBar")
-    const css = document.getElementById("cssBar")
-    const ruby = document.getElementById("rubyBar")
-    const ui = document.getElementById("uiBar")
-    const photoshop = document.getElementById("photoshopBar")
-    const illustrator = document.getElementById("illustratorBar")
-    let width = 0
-
-    const id_90 = setInterval(() => {
-      if (width >= 75) {
-        clearInterval(id_90);
-      } else{
-        width++;
-        html.style.width = width + '%';
-        html.innerHTML = width * 1 + 15 + '%';
-        css.style.width = width + '%';
-        css.innerHTML = width * 1 + 15 + '%';
-      }
-    }, 25)
-
-    const id_80 = setInterval(() => {
-      if (width >= 65) {
-        clearInterval(id_80);
-      } else{
-        width++;
-        react.style.width = width + '%';
-        react.innerHTML = width * 1 + 15 + '%';
-      }
-    }, 20)
-
-    const id_70 = setInterval(() => {
-      if (width >= 55) {
-        clearInterval(id_70);
-      } else{
-        width++;
-        ruby.style.width = width + '%';
-        ruby.innerHTML = width * 1 + 15 + '%';
-        js.style.width = width + '%';
-        js.innerHTML = width * 1 + 15 + '%';
-        ui.style.width = width + '%';
-        ui.innerHTML = width * 1 + 15 + '%';
-      }
-    }, 15)
-
-    const id_60 = setInterval(() => {
-      if (width >= 45) {
-        clearInterval(id_60);
-      } else{
-        width++;
-        photoshop.style.width = width + '%';
-        photoshop.innerHTML = width * 1 + 15 + '%';
-        illustrator.style.width = width + '%';
-        illustrator.innerHTML = width * 1 + 15 + '%';
-      }
-    }, 30)
-  }
-
-
   render(){
     console.log('about props', this.props);
     return (
@@ -85,86 +25,14 @@ class About extends React.Component {
 
         <Container>
           <h1 className='page-title'>About Me</h1>
-          <Grid stackable columns={2}>
-            <Grid.Column width={7} className='photos-container'>
+          <Grid stackable columns={3}>
+            <Grid.Column width={1}/>
+            <Grid.Column width={4} className='photos-container'>
               <Segment>
-                <div className='photo-1'/>
+                <div className='profile-pic'>
+                  <img src={profilePic} alt='profile' />
+                </div>
               </Segment>
-            </Grid.Column>
-
-            <Grid.Column width={9} className='skills-container'>
-              <Grid stackable columns={2}>
-                <Grid.Column width={3}>
-                  <div className='js label'>JavaScript</div>
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  { this.props.status && <ProgressTemplate percent={70} /> }
-                </Grid.Column>
-              </Grid>
-              <Grid stackable columns={2}>
-                <Grid.Column width={3}>
-                  <div className='react label'>React</div>
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  { this.props.status && <ProgressTemplate percent={80} /> }
-                </Grid.Column>
-              </Grid>
-              <Grid stackable columns={2}>
-                <Grid.Column width={3}>
-                  <div className='ruby label'>Ruby/Rails</div>
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  { this.props.status && <ProgressTemplate percent={70} /> }
-                </Grid.Column>
-              </Grid>
-              <Grid stackable columns={2}>
-                <Grid.Column width={3}>
-                  <div className='html label'>HTML</div>
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  { this.props.status && <ProgressTemplate percent={90} /> }
-                </Grid.Column>
-              </Grid>
-              <Grid stackable columns={2}>
-                <Grid.Column width={3}>
-                  <div className='css label'>CSS</div>
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  { this.props.status && <ProgressTemplate percent={90} /> }
-                </Grid.Column>
-              </Grid>
-              <Grid stackable columns={2}>
-                <Grid.Column width={3}>
-                  <div className='ui-design label'>UI Design</div>
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  { this.props.status && <ProgressTemplate percent={80} /> }
-                </Grid.Column>
-              </Grid>
-              <Grid stackable columns={2}>
-                <Grid.Column width={3}>
-                  <div className='photoshop label'>Photoshop</div>
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  { this.props.status && <ProgressTemplate percent={70} /> }
-                </Grid.Column>
-              </Grid>
-              <Grid stackable columns={2}>
-                <Grid.Column width={3}>
-                  <div className='illustrator label'>Illustrator</div>
-                </Grid.Column>
-                <Grid.Column width={13}>
-                  { this.props.status && <ProgressTemplate percent={70} /> }
-                </Grid.Column>
-              </Grid>
-
-
-
-              
-            </Grid.Column>
-          </Grid>
-          <div className='about-me-container'>
-            <div>
               <div className='icon-container'>
                 <a href='https://www.linkedin.com/in/soyeongoh/' target='_blank' rel='noopener noreferrer'>
                   <i class="fab fa-linkedin"></i>
@@ -173,11 +41,89 @@ class About extends React.Component {
                   <i class="fab fa-github-square"></i>
                 </a>
                 <a href='https://docs.google.com/document/d/1AYRvCdKcBvNb_3NKwQ2xADx076ZHQkcY-FyHO7vguKE/edit?usp=sharing' target='_blank' rel='noopener noreferrer'>
-                  <Button>Resume</Button>
+                  <Button basic color='green' size='tiny'>Resume</Button>
                 </a>
               </div>
+            </Grid.Column>
+
+            <Grid.Column width={10} className='skills-container'>
+              <Grid stackable columns={2}>
+                <Grid.Column width={3} className='skill-name'>
+                  <div className='js label'>JavaScript</div>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  { this.props.status && <ProgressTemplate percent={70} /> }
+                </Grid.Column>
+              </Grid>
+              <Grid stackable columns={2}>
+                <Grid.Column width={3} className='skill-name'>
+                  <div className='react label'>React</div>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  { this.props.status && <ProgressTemplate percent={80} /> }
+                </Grid.Column>
+              </Grid>
+              <Grid stackable columns={2}>
+                <Grid.Column width={3} className='skill-name'>
+                  <div className='ruby label'>Ruby/Rails</div>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  { this.props.status && <ProgressTemplate percent={70} /> }
+                </Grid.Column>
+              </Grid>
+              <Grid stackable columns={2}>
+                <Grid.Column width={3} className='skill-name'>
+                  <div className='html label'>HTML</div>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  { this.props.status && <ProgressTemplate percent={90} /> }
+                </Grid.Column>
+              </Grid>
+              <Grid stackable columns={2}>
+                <Grid.Column width={3} className='skill-name'>
+                  <div className='css label'>CSS</div>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  { this.props.status && <ProgressTemplate percent={90} /> }
+                </Grid.Column>
+              </Grid>
+              <Grid stackable columns={2}>
+                <Grid.Column width={3} className='skill-name'>
+                  <div className='ui-design label'>UI Design</div>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  { this.props.status && <ProgressTemplate percent={80} /> }
+                </Grid.Column>
+              </Grid>
+              <Grid stackable columns={2}>
+                <Grid.Column width={3} className='skill-name'>
+                  <div className='photoshop label'>Photoshop</div>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  { this.props.status && <ProgressTemplate percent={70} /> }
+                </Grid.Column>
+              </Grid>
+              <Grid stackable columns={2}>
+                <Grid.Column width={3} className='skill-name'>
+                  <div className='illustrator label'>Illustrator</div>
+                </Grid.Column>
+                <Grid.Column width={13}>
+                  { this.props.status && <ProgressTemplate percent={70} /> }
+                </Grid.Column>
+              </Grid>
+
+            </Grid.Column>
+            <Grid.Column width={1}/>
+          </Grid>
+
+          <div className='about-me-container'>
+            <Grid stackable columns={3}>
+              <Grid.Column width={1}/>
+              <Grid.Column width={14}>
               <p>Full stack software engineer with a passion for continuous learning and building a sophisticated web application. With experience in Ruby on Rails, JavaScript, React and CSS3 and background as a web designer, I discovered web development through utilizing my skill to visualize web apps. I'm a strong collaborator and I bring exceptional skills in problem-solving and communication through extensive experiences from online marketing and customer service.</p>
-            </div>
+              </Grid.Column>
+              <Grid.Column width={1}/>
+            </Grid>
           </div>
         </Container>
       </div>
