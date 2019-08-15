6c54029b019b2fce994747697c483ddb707f9e25
axionos <axionoso@gmail.com>
2019-08-15 13:20:03 -0400

    add to top button

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { Container }from 'semantic-ui-react'
+import { Link } from 'react-scroll'
 
 class Blog extends React.Component {
   render(){
     return (
       <div className="blog" id='blog'>
         <Container>
-          <div className='pg-title-holder'>
-            <h1 className='page-title'>
-              Blog
-              <div className='title-bg'/>
-            </h1>
-          </div>
-          <div id="medium-widget"></div>
-          <div className='to-the-top'></div>
+            <div className='pg-title-holder'>
+              <h1 className='page-title'>
+                Blog
+                <div className='title-bg'/>
+              </h1>
+            </div>
+            <div id="medium-widget"></div>
+            <div className='flex'>
+              <span className='to-the-top'>
+                <Link
+                  activeClass="active"
+                  to="intro"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration= {500}
+                  className='link'
+                >
+                  <i class="fas fa-chevron-up"></i>
+                  <div>Go to Top</div>
+                </Link>
+
+              </span>
+            </div>
         </Container>
       </div>
     );
